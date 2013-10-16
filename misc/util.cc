b874d16f2fd819b7c21fd911493495c510ef6e9d
Takanori MAEHARA <maehara@prefield.com>
2013-10-17 08:45:06 +0900

    Create util.cc

---
+++ misc/util.cc
@@ -0,0 +1,17 @@
+
+template <class T>
+struct Dictionary {
+  unordered_map<T, size_t> dict;
+  vector<T> idict;
+  size_t id(T s) { 
+    if (!dict.count(s)) {
+      dict[s] = idict.size();
+      idict.push_back(s);
+    }
+    return dict[s];
+  }
+  T value(size_t id) {
+    return idict[id];
+  }
+};
+
