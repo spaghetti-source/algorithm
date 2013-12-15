58c01e6457a4b56340c3debf49c95e850e06f940
Takanori MAEHARA <maehara@prefield.com>
2013-12-15 23:38:28 +0900

    Enumerable Union Find (amortized O(log n))

---
+++ datastructure/union_find2.cc
@@ -0,0 +1,24 @@
+struct union_find {
+  int components;
+  vector<int> root, next, size;
+  void clear(int n) {
+    root.resize(n);
+    iota(root.begin(), root.end(), 0);
+    next.assign(n, -1);
+    size.assign(n,  1);
+  }
+  bool find(int x, int y) { return root[x] == root[y]; }
+  bool unite(int x, int y) {
+    if ((x = root[x]) == (y = root[y])) return false;
+    if (size[x] < size[y]) swap(x, y);
+    size[x] += size[y];
+    for (int z; y >= 0; y = z) {
+      z = next[y];
+      next[y] = next[x];
+      next[x] = y;
+      root[y] = x;
+    }
+    --components;
+    return true;
+  }
+};
