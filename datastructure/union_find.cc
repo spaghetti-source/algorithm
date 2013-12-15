ee2d6561893891f5e8a573152d3a976eb5a0e0f6
Takanori MAEHARA <maehara@prefield.com>
2013-12-15 23:39:25 +0900

    Union Find (amortized O(a^{-1}(n)))

---
+++ datastructure/union_find.cc
@@ -0,0 +1,13 @@
+struct union_find {
+  vector<int> p; 
+  union_find(int n) : p(n, -1) { };
+  bool unite(int u, int v) { 
+    if ((u = root(u)) == (v = root(v))) return false;
+    if (p[u] > p[v]) swap(u, v);
+    p[u] += p[v]; p[v] = u;
+    return true;
+  }
+  bool find(int u, int v) { return root(u) == root(v); }
+  int root(int u) { return p[u] < 0 ? u : p[u] = root(p[u]); }
+  int size(int u) { return -p[root(u)]; }
+};
