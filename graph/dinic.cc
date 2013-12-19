91a36b0c7bcc49ff3c027bff0b142cf383f792b1
Takanori MAEHARA <maehara@prefield.com>
2013-12-19 19:41:25 +0900

    Maximum Flow (Dinic)

--- graph/dinic.cc
+++ graph/dinic.cc
@@ -22,13 +22,12 @@
 //
 // Verified:
 //   PKU 1459, Power Network
-//
+//   SPOJ 4410, Fast Maximum Flow
 //
 // References:
 //   E. A. Dinic (1970):
 //   Algorithm for solution of a problem of maximum flow in networks with power estimation.
 //   Soviet Mathematics Doklady, vol. 11, pp. 1277-1280.
-
 
 #include <iostream>
 #include <vector>
@@ -38,82 +37,87 @@
 
 using namespace std;
 
-int n;
+typedef long long flow_type;
+flow_type INF = (1ULL<<50);
 struct edge {
   int src, dst;
-  double residue;
+  flow_type residue;
   int rev;
 };
-vector< vector<edge> > adj;
-void add_edge(int u, int v, double c) {
-  adj[u].push_back((edge){u, v, c, adj[v].size()});
-  adj[v].push_back((edge){v, u, 0, adj[u].size()-1});
-}
-vector<int> level, iter;
-double augment(int u, int t, double cur) {
-  if (u == t) return cur;
-  for (int &i = iter[u]; i < adj[u].size(); ++i) {
-    edge &e = adj[u][i];
-    if (e.residue > 0 && level[u] < level[e.dst]) {
-      double f = augment(e.dst, t, min(cur, e.residue));
-      if (f > 0) {
-        e.residue -= f;
-        adj[e.dst][e.rev].residue += f;
-        return f;
-      }
+struct maximum_flow {
+  vector<edge> edges;
+  void add_edge(int src, int dst, flow_type capacity) {
+    edges.push_back({src, dst, capacity});
+  }
+  int n;
+  vector<vector<edge>> adj;
+  void make_graph(int n_ = 0) {
+    n = n_;
+    for (auto e: edges) 
+      n = max(n, max(e.src, e.dst)+1);
+    adj.resize(n);
+    for (auto e: edges) {
+      edge r = {e.dst, e.src, 0}; 
+      adj[e.src].push_back(e);
+      adj[r.src].push_back(r);
+      adj[e.src].back().rev = adj[e.dst].size()-1;
+      adj[r.src].back().rev = adj[r.dst].size()-1;
     }
   }
-  return 0;
-}
-int bfs(int s, int t) {
-  level.assign(n, -1);
-  level[s] = 0;
-  queue<int> Q;
-  Q.push(s);
-  while (!Q.empty()) {
-    int u = Q.front(); Q.pop();
-    for (int i = 0; i < adj[u].size(); ++i) {
+  vector<int> level, iter;
+  flow_type augment(int u, int t, flow_type cur) {
+    if (u == t) return cur;
+    for (int &i = iter[u]; i < adj[u].size(); ++i) {
       edge &e = adj[u][i];
-      if (e.residue > 0 && level[e.dst] < 0) {
-        Q.push(e.dst);
-        level[e.dst] = level[u] + 1;
+      if (e.residue > 0 && level[u] < level[e.dst]) {
+        flow_type f = augment(e.dst, t, min(cur, e.residue));
+        if (f > 0) {
+          e.residue -= f;
+          adj[e.dst][e.rev].residue += f;
+          return f;
+        }
       }
     }
+    return 0;
   }
-  return level[t];
-}
-double maximum_flow(int s, int t) {
-  double flow = 0;
-  while (1) {
-    if (bfs(s, t) < 0) return flow;
-    iter.assign(n, 0);
-    for (double f; (f = augment(s, t, 1.0/0.0)) > 0; ) 
-      flow += f;
+  int bfs(int s, int t) {
+    level.assign(n, -1);
+    level[s] = 0;
+    queue<int> Q;
+    Q.push(s);
+    while (!Q.empty()) {
+      int u = Q.front(); Q.pop();
+      if (u == t) break;
+      for (int i = 0; i < adj[u].size(); ++i) {
+        edge &e = adj[u][i];
+        if (e.residue > 0 && level[e.dst] < 0) {
+          Q.push(e.dst);
+          level[e.dst] = level[u] + 1;
+        }
+      }
+    }
+    return level[t];
   }
-}
+  flow_type solve(int s, int t) {
+    flow_type flow = 0;
+    while (bfs(s, t) >= 0) {
+      iter.assign(n, 0);
+      for (flow_type f; (f = augment(s, t, INF)) > 0; )
+        flow += f;
+    }
+    return flow;
+  }
+};
 
 int main() {
-  for (int np, nc, m; scanf("%d %d %d %d", &n, &np, &nc, &m) == 4; ) {
-    int s = n, t = n+1;
-    n += 2;
-    adj.clear();
-    adj.resize(n);
-    for (int i = 0; i < m; ++i){
-      int src, dst, cap;
-      scanf(" (%d ,%d )%d", &src, &dst, &cap);
-      add_edge(src, dst, cap);
-    }
-    for (int i = 0; i < np; ++i){
-      int dst, cap;
-      scanf(" ( %d )%d", &dst, &cap);
-      add_edge(s, dst, cap);
-    }
-    for(int i = 0; i < nc; ++i){
-      int src, cap;
-      scanf(" ( %d )%d ", &src, &cap);
-      add_edge(src, t, cap);
-    }
-    double w = maximum_flow(s, t);
-    printf("%d\n", (int)w);
+  int n, m; 
+  scanf("%d %d", &n, &m);
+  maximum_flow mf;
+  for (int i = 0; i < m; ++i) {
+    int u, v, w;
+    scanf("%d %d %d", &u, &v, &w);
+    mf.add_edge(u-1, v-1, w);
   }
+  mf.make_graph(n);
+  printf("%lld\n", mf.solve(0, n-1));
 }
