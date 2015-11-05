8df9b92e1e5a11c563550b6fa5cbc23a729ddeb0
Takanori MAEHARA <maehara@prefield.com>
2015-11-05 18:04:31 +0900

    Horowitz and Sahni's subset sum in O(n 2^{n/2}) time/space

--- other/subset_sum.cc
+++ other/subset_sum.cc
@@ -16,10 +16,20 @@
 //   O(n 2^{n/2}) = O(n 1.41^n), which is practical for n <= 45.
 //   If all values are bounded, it also runs in O(n^2 amax).
 //
+// Remark:
+//   Schroeppel and Shamir improved the space complexity in O(2^{n/4}).
+//   It generates L/R in the increasing/decreasing order by using O(2^{n/4}) space:
+//   Split a into four component, generate all subset sums, and use priority queue.
+//   However, in my implementation (see below), it is much slower than the H-S algorithm.
+//
 // References:
 //   E. Horowitz and S. Sahni (1974):
 //   "Computing Partitions with Applications to the Knapsack Problem",
 //   Journal of the ACM., vol. 21, no. 2, pp. 277-292.
+//
+//   R. Schroeppel and A. Shamir (1981):
+//   " A T = O(2n=2); S = O(2n=4) algorithm for certain NP-Complete Problems",
+//   SIAM Journal of Computing, vol. 10, no. 3, pp. 456-464.
 //
 #include <iostream>
 #include <vector>
@@ -86,6 +96,52 @@ long long subset_sum_count(const vector<T>& x, const T &sum) {
       ans += p.snd * R[sum - p.fst];
   return ans;
 }
+
+// low memory usage but slow :(
+template <class T>
+bool subset_sum_lowmemory(const vector<T>& x, const T &sum) {
+
+  vector<T> X = {0}, Y = {0}, Z = {0}, W = {0};
+  function<bool(int,int,vector<T>&)> rec = 
+    [&](int i, int j, vector<T> &X) {
+    for (; i < j; ++i) {
+      int n = X.size();
+      for (int k = 0; k < n; ++k) {
+        T a = x[i] + X[k];
+        if (a == sum) return true;
+        if (!binary_search(all(X), a)) 
+          X.push_back(a);
+      }
+      inplace_merge(X.begin(), X.begin()+n, X.end());
+    }
+    return false;
+  };
+  int n = x.size();
+  if (rec(0*n/4, 1*n/4, X) || rec(1*n/4, 2*n/4, Y) || 
+      rec(2*n/4, 3*n/4, Z) || rec(3*n/4, 4*n/4, W)) return true;
+
+  typedef tuple<T,int,int> node;
+  priority_queue<node, vector<node>, less<node>> P;
+  priority_queue<node, vector<node>, greater<node>> Q;
+  for (int i = 0; i < X.size(); ++i) 
+    P.push(make_tuple(X[i]+Y.back(), i, Y.size()-1));
+  for (int i = 0; i < Z.size(); ++i)
+    Q.push(make_tuple(Z[i]+W[0], i, 0));
+
+  while (!P.empty() && !Q.empty()) {
+    T a = get<0>(P.top()), b = get<0>(Q.top());
+    if (a + b == sum) return true;
+    if (a + b > sum) { 
+      int i = get<1>(P.top()), j = get<2>(P.top()); P.pop();
+      if (--j >= 0) P.push(make_tuple(X[i]+Y[j], i, j));
+    } else {
+      int i = get<1>(Q.top()), j = get<2>(Q.top()); Q.pop();
+      if (++j < W.size()) Q.push(make_tuple(Z[i]+W[j], i, j));
+    }
+  }
+  return false;
+}
+
 
 
 // === tick a time ===
