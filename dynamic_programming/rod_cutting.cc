8b342f830e91945887b154ded00096bf1e813d92
Xiaohui Wang <wang502@indiana.edu>
2016-04-08 17:51:56 -0400

    Rod cutting (dynamic programming)

--- dynamic_programming/rod_cutting.cc
+++ dynamic_programming/rod_cutting.cc
@@ -1,4 +1,15 @@
 // Rod Cutting Problem
+// Description:
+//   Given an array of values and a rod length, find the maximum value we can get by doing the rod cutting
+// Algorithm:
+//   let di denotes the maximum value we can get from cutting a rod of length i, vi denotes the value of a cut of length i
+//   then
+//       di = 0, when i = 0;
+//       di = max(di-k + vk) for 1<=k<=i, when i>=1
+// Complexity:
+//    let m be the number of different kinds of cut, let n be the rod length
+//    time complexity: O(mn)
+//    Space complexity: O(n)
 
 #include <iostream>
 #include <cmath>
@@ -23,6 +34,8 @@ int rod_cut(int v[], int vsize, int len){
 }
 
 // output the patterns of cutting that can generates the maximum value
+// pred[] keeps track of the cut of each length
+// pred[i] denotes the cut made when the rod length is i
 int* rod_cutting_witness(int v[], int vsize, int len){
   int values[len+1];
   values[0] = 0;
