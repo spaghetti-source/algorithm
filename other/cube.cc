6d97b64b63671811a45140238667266f86d5d229
Takanori MAEHARA <maehara@prefield.com>
2016-07-15 14:19:18 +0900

    Cube (or Dice) data structure

--- other/cube.cc
+++ other/cube.cc
@@ -1,8 +1,8 @@
 //
-// Cube data structure
+// Cube container
 //
 // Descrption:
-//   A data structure for cube has six faces on
+//   It has a cube container which has six faces on
 //   front, up, down, left, right, and bottom.
 //   It admits the following three rotations
 //     rotX: front -> up    -> back  -> down
@@ -29,13 +29,57 @@ using namespace std;
 
 template <class T>
 struct cube {
-  T F, U, D, L, R, B;
-  void rotX() { swap(D, B); swap(B, U); swap(U, F); } // FUBD -> DFUB
-  void rotY() { swap(D, R); swap(R, U); swap(U, L); } // LURD -> DLUR
-  void rotZ() { swap(B, R); swap(R, F); swap(F, L); } // LFRB -> BLFR
+  T F, B, U, D, L, R;
+  void rotX() { T x = D; D = B; B = U; U = F; F = x; } // FUBD -> DFUB
+  void rotY() { T x = D; D = R; R = U; U = L; L = x; } // LURD -> DLUR
+  void rotZ() { T x = B; B = R; R = F; F = L; L = x; } // LFRB -> BLFR
+
+  // usage:
+  // for (int i = 0; i < 24; ++i) {
+  //   /* do something */
+  //   c.next_roll();
+  // }
+  //
+  // or 
+  //
+  // do {
+  //   /* do something */
+  // } while (c.next_roll());
+  bool next_roll() { 
+    static int it = 0;
+    rotZ();
+    if (it % 8 == 3) rotX();
+    if (it % 8 == 7) rotY();
+    return it = (it == 23 ? 0 : it+1);
+  }
+  bool operator==(cube c) const {
+    for (int k = 0; k < 6; ++k) {
+      if (U != c.U || F != c.F) continue;
+      for (int i = 0; i < 4; ++i) {
+        if (L == c.L && F == c.F && R == c.R && L == c.R) return true;
+        c.rotZ();
+      }
+      if (k % 2) c.rotY(); else c.rotZ();
+    }
+    return false;
+  }
 };
+template <class T>
+ostream &operator<<(ostream &ofs, cube<T> c) {
+  return (ofs << c.F << c.B << c.U << c.D << c.L << c.R);
+}
+
+void test() {
+  cube<int> c = {1,2,3,4,5,6};
+  do {
+    cout << c << endl;
+  } while (c.next_roll());
+}
 
 int main() {
+  test();
+  return 0;
+
   int ncase; scanf("%d", &ncase);
   for (int icase = 0; icase < ncase; ++icase) {
     cube<int> c;
