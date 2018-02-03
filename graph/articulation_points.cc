400f2f633d18508288a45307d32d258188dfb38b
Takanori MAEHARA <maehara@prefield.com>
2018-02-04 04:23:02 +0900

    Hopcroft-Tarjan's Articulation Point / Biconnected Components

--- graph/articulation_points.cc
+++ graph/articulation_points.cc
@@ -1,5 +1,5 @@
 // 
-// Articulation points / Biconnected components
+// Block-Cut Tree (Articulation points / Biconnected components)
 //
 // Description:
 //   Let G = (V, E). If G-v is disconnected, v in V is said to
@@ -7,25 +7,19 @@
 //   it is said to be biconnected.
 //
 //   A biconnected component is a maximal biconnected subgraph.
-//   The algorithm finds all articulation points and biconnected 
-//   components.
+//   The algorithm finds all articulation points and biconnected
+//   components. It can be obtained by the Hopcroft-Tarjan DFS.
 //
-//   The most important fact is that by contracting biconnected
-//   components we obtain a tree, which is called the block tree.
-//
-//
-// Algorithm:
-//   Hopcroft-Tarjan's DFS based algorithm.
-//
-//   Single DFS finds a block tree rooted from the component
-//   that contains the specified root.
+//   We maintain the biconnected component decomposition by the
+//   block tree whose vertices are the blocks and the articulation
+//   points. By contracting the graph by the articulation points,
+//   we obtain the intersection graph of the blocks.
 //
 // Complexity:
 //   O(n + m).
 //
 // Verified:
-//   SPOJ 14956: Submerging Island (articulation point)
-//   POJ 2942: Knights of the Round Table (biconnected components)
+//   AOJ_GRL_3_A (articulation point)
 //
 // References:
 //   J. Hopcroft and R. E. Tarjan (1973):
@@ -33,68 +27,107 @@
 //   Communications of the ACM, vol.16, no.6, pp.372-378.
 //
 
-#include <iostream>
-#include <vector>
-#include <cstdio>
-#include <algorithm>
-#include <functional>
-#include <unordered_set>
+// g++ -std=c++17 -O3 -fmax-errors=1 -fsanitize=undefined
+#include <bits/stdc++.h>
 
 using namespace std;
 
 #define fst first
 #define snd second
 #define all(c) ((c).begin()), ((c).end())
+#define TEST(s) if (!(s)) { cout << __LINE__ << " " << #s << endl; exit(-1); }
 
-struct graph {
+struct Graph {
   int n;
   vector<vector<int>> adj;
-  graph(int n) : n(n), adj(n) { }
-  void add_edge(int src, int dst) {
-    adj[src].push_back(dst);
-    adj[dst].push_back(src);
-  }
-
-  void biconnected_components() {
-    vector<int> num(n), low(n), S;
-    unordered_set<int> arts;
-
-    function<void(int,int,int&)> dfs = [&](int p, int u, int &t) { 
-      num[u] = low[u] = ++t; 
-      S.push_back(u);
-      for (int v: adj[u]) {
-        if (v == p) continue;
-        if (num[v] == 0) {
-          dfs(u, v, t);
-          low[u] = min(low[u], low[v]);
-          if (num[u] <= low[v]) {
-            if (num[u] != 1 || num[v] > 2) {
-              // here, u is an articulation point if
-              //   (a). u is non-root
-              //   (b). u is root with two more children 
-            }
-            vector<int> C = {u}; // biconnected component
-            while (C.back() != v) {
-              C.push_back(S.back());
-              S.pop_back();
-            }
-          }
-        } else low[u] = min(low[u], num[v]);
-      }
-    };
-    for (int u = 0, t; u < n; ++u) 
-      if (!num[u]) dfs(-1, u, t = 0);
-    cout << arts.size() << endl;
+  Graph(int n) : n(n), adj(n) { }
+  void addEdge(int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
   }
 };
 
-int main() {
-  for (int n, m; ~scanf("%d %d", &n, &m) && n; ) {
-    graph g(n);
-    for (int i = 0; i < m; ++i) {
-      int u, v; scanf("%d %d", &u, &v);
-      g.add_edge(u-1, v-1);
+//
+// bcc.n-bcc.block.size() is the number of articulation points
+// index[u] is the corresponding node in the block-cut tree.
+// Here, if u in block[k] then
+//   if u is an articulation point, k in adj[index[u]] 
+//   otherwise, index[u] = k
+//
+struct BiconnectedComponents : Graph {
+  vector<int> is_articulation, index;
+  vector<vector<int>> block;
+
+  BiconnectedComponents(Graph g) : Graph(0) {
+    vector<int> low(g.n), num(g.n), cur(g.n), par(g.n, -1), path;
+    is_articulation.resize(g.n);
+    for (int s = 0; s < g.n; ++s) {
+      if (num[s]) continue;
+      int time = 0;
+      vector<int> stack = {s};
+      while (!stack.empty()) {
+        int u = stack.back();
+        if (cur[u] == 0) {
+          low[u] = num[u] = ++time;
+          path.push_back(u);
+        }
+        if (cur[u] == g.adj[u].size()) {
+          stack.pop_back();
+        } else if (cur[u] >= 0) {
+          int v = g.adj[u][cur[u]++];
+          if (num[v] == 0) {
+            cur[u] = ~cur[u];
+            stack.push_back(v);
+          } else if (v != par[u]) {
+            low[u] = min(low[u], num[v]);
+          }
+        } else {
+          cur[u] = ~cur[u];
+          int v = g.adj[u][cur[u]-1];
+          low[u] = min(low[u], low[v]);
+          if (num[u] <= low[v]) {
+            is_articulation[u] = (num[u] > 1 || num[v] > 2);
+            block.push_back({u});
+            while (block.back().back() != v) {
+              block.back().push_back(path.back());
+              path.pop_back();
+            }
+          }
+        }
+      }
     }
-    g.biconnected_components();
+    index.resize(g.n); // make a block tree
+    n = block.size();
+    for (int u = 0; u < g.n; ++u) 
+      if (is_articulation[u]) index[u] = n++;
+    adj.resize(n);
+    for (int k = 0; k < block.size(); ++k) {
+      for (int u: block[k]) {
+        if (!is_articulation[u]) index[u] = k;
+        else addEdge(k, index[u]);
+      }
+    }
   }
+};
+
+void AOJ_GRL_3_A() {
+  int n, m; 
+  scanf("%d %d", &n, &m);
+  Graph g(n);
+  for (int i = 0; i < m; ++i) {
+    int u, v; scanf("%d %d", &u, &v);
+    g.addEdge(u, v);
+  }
+  BiconnectedComponents bcc(g);
+  for (int u = 0; u < g.n; ++u) 
+    if (bcc.is_articulation[u]) cout << u << endl;
 }
+
+int main() {
+  AOJ_GRL_3_A();
+  //SPOJ_SUBMERGE();
+  //test();
+  /*
+  */
+}
+
